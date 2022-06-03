1fb4d847dbe1a2e6124d8457c6e35f075514a94c
Invisible EInsein <91374376+invisibleeinstei@users.noreply.github.com>
2022-06-03 11:18:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/ca067dc4472190cbe4af4.mp4 https://telegra.ph/file/47846a78a6eed2d527f80.mp4 https://telegra.ph/file/a1283bfa2ff64c681dedc.mp4')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/4d2b55505d04f5c5a6ab1.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
