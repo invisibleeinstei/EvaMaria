3b48de1c4eabf0c57b4285cb4528e255e3966b51
Invisible EInsein <91374376+invisibleeinstei@users.noreply.github.com>
2022-07-11 21:02:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
-from utils import IMDb
+from utils import imdb
 
 class Bot(Client):
 
