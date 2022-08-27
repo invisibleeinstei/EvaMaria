85a282098cb5fd6905f821a0a4fb7d7912fbfdf5
Invisible EInsein <91374376+invisibleeinstei@users.noreply.github.com>
2022-08-27 20:39:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,10 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>ඔයා ඉල්ලුවේ =<code>{search}</code></b>\n\n<a href="https://t.me/+nYWmNNoj3QFiYWU1">AL KUPPIYA GROUP 🐣</a>\n<a href="https://t.me/+bPFNYjdECxRjYTQ1">AL KUPPIYA OFFICIAL CHANNEL 👾</a>"
+        cap = f"<b>ඔයා ඉල්ලුවේ =<code>{search}</code></b>
+        
+        <a href="https://t.me/+nYWmNNoj3QFiYWU1">AL KUPPIYA GROUP 🐣</a>
+        <a href="https://t.me/+bPFNYjdECxRjYTQ1">AL KUPPIYA OFFICIAL CHANNEL 👾</a>"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
